cff9ebd4b78947c12b013027cd10d866813b06fc
Vladimir Sigalkin <vladimir@sigalkin.ru>
2019-01-12 15:40:35 +0300

    Added category icon

---
+++ Assets/extOSC.PlayMaker/Scripts/Editor/OSCPlayMakerIcon.cs
@@ -0,0 +1,27 @@
+﻿/* Copyright (c) 2019 ExT (V.Sigalkin) */
+
+#if EXTOSC && PLAYMAKER
+
+using UnityEditor;
+
+using HutongGames.PlayMakerEditor;
+
+using extOSC.Editor;
+
+namespace extOSC.PlayMaker.Editor
+{
+	[InitializeOnLoad]
+	public class OSCPlayMakerIcon
+	{
+        #region Static Constructor
+
+		static OSCPlayMakerIcon()
+		{
+			Actions.AddCategoryIcon("extOSC", OSCEditorTextures.IronWallSmall);
+		}
+
+        #endregion
+    }
+}
+
+#endif
