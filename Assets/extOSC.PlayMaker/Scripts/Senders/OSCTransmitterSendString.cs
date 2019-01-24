946d534c4ce9adcc3113ada69c7ca6ad0a0f140a
Vladimir Sigalkin <_ext_@bk.ru>
2019-01-24 15:45:03 +0300

    Added SenderString.

---
+++ Assets/extOSC.PlayMaker/Scripts/Senders/OSCTransmitterSendString.cs
@@ -0,0 +1,22 @@
+﻿/* Copyright (c) 2019 ExT (V.Sigalkin) */
+
+#if EXTOSC && PLAYMAKER
+
+using HutongGames.PlayMaker;
+
+namespace extOSC.PlayMaker.Events
+{
+	public class OSCTransmitterSendString : OSCTransmitterSend<FsmString, string>
+	{
+        #region Protected Methods
+
+		protected override void FillMessage(OSCMessage message, string value)
+		{
+			message.AddValue(OSCValue.String(value));
+		}
+
+        #endregion
+    }
+}
+
+#endif
